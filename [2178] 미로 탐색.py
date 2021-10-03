38f034321490148d6d95eb8f0a6a8a8bb0321c46
CuriHuS <64942546+CuriHuS@users.noreply.github.com>
2021-10-03 12:39:42 +0900

    Update [2178] 미로 탐색.py

--- [2178] 미로 탐색.py
+++ [2178] 미로 탐색.py
@@ -8,8 +8,17 @@ n,m = map(int,input().split())
 for _ in range(n):
     graph.append(list(map(int,input())))
 v,i=0,0
+dx=[-1,1,0,0]
+dy=[0,0,1,-1]
+
 def bfs(v,i):
     print(graph)
+    for a in range(3):
+        if v+dx[a]
+
+
+
+
     try:
         if graph[v+1][i] == 1:
             graph[v+1][i]+=graph[v][i]
@@ -29,3 +38,4 @@ def bfs(v,i):
 bfs(v,i)
 
 # 문제점: if 단계에서 bfs 재귀함수가 걸리면서 if 한번하고 재귀걸리고 다른 if가서 또 재귀함. -> 이거 
+
