e61fd6ee1141c46f98f27203416a250fa7dfbec2
CuriHuS <64942546+CuriHuS@users.noreply.github.com>
2021-10-08 23:11:58 +0900

    Update [2178] 미로 탐색.py

--- [2178] 미로 탐색.py
+++ [2178] 미로 탐색.py
@@ -7,35 +7,55 @@ graph=[]
 n,m = map(int,input().split())
 for _ in range(n):
     graph.append(list(map(int,input())))
-v,i=0,0
+x,y=0,0
 dx=[-1,1,0,0]
 dy=[0,0,1,-1]
 
 def bfs(v,i):
     print(graph)
     for a in range(3):
-        if v+dx[a]
-
-
-
-
-    try:
-        if graph[v+1][i] == 1:
-            graph[v+1][i]+=graph[v][i]
-            bfs(v+1,i)
-        if graph[v][i+1] == 1:
-            graph[v][i+1]+=graph[v][i]
-            bfs(v,i+1)
-        if graph[v-1][i] == 1:
-            graph[v-1][i]+=graph[v][i]
-            bfs(v-1,i)
-        if graph[v][i-1] == 1:
-            graph[v][i-1]+=graph[v][i]
-            bfs(v,i-1)
-    except:
-        0
+        print(v,i,a)
+        
+        if 0<=  v+dx[a] < n and 0<=i+dy[a] < m:
+            if graph[v+dx[a]][i+dy[a]] == 1:
+                graph[v+dx[a]][i+dy[a]] += graph[v][i]
+                bfs(v+dx[a],i+dy[a])
+        else:
+            continue
             
-bfs(v,i)
+bfs(x,y)
 
-# 문제점: if 단계에서 bfs 재귀함수가 걸리면서 if 한번하고 재귀걸리고 다른 if가서 또 재귀함. -> 이거 
+print(graph[n-1][m-1])
 
+### 정답 ###
+"""
+from collections import deque
+import sys
+read = sys.stdin.readline
+
+
+graph=[]
+n,m = map(int,input().split())
+for _ in range(n):
+    graph.append(list(map(int,input())))
+x,y=0,0
+dx=[-1,1,0,0]
+dy=[0,0,1,-1]
+
+def bfs(v,i):
+    q=deque()
+    q.append((v,i))
+    while q:
+        v, i = q.popleft()
+        for a in range(4):
+            if 0<=  v+dx[a] < n and 0<=i+dy[a] < m:
+                if graph[v+dx[a]][i+dy[a]] == 1:
+                    graph[v+dx[a]][i+dy[a]] += graph[v][i]
+                    q.append((v+dx[a],i+dy[a]))
+            else:
+                continue
+            
+bfs(x,y)
+
+print(graph[n-1][m-1])
+"""
