88c84dc677e03cece608f9a190ba540337747e46
CuriHuS <64942546+CuriHuS@users.noreply.github.com>
2021-10-17 17:50:55 +0900

    Update [7576] 토마토.py

--- 그래프 이론, 탐색/[7576] 토마토.py
+++ 그래프 이론, 탐색/[7576] 토마토.py
@@ -2,36 +2,52 @@ from collections import deque
 import sys
 m,n= map(int,input().split())
 graph=[]
-for _ in range(n):
-    graph.append(list(map(int,sys.stdin.readline().split())))
+a=3
+S=deque()
+for k in range(n):
+    _ = list(map(int,sys.stdin.readline().split()))
+    if 0 in _:
+        a=0
+    if 1 in _:
+        for i in range(m):
+            if _[i]==1:
+                S.append((k,i))
+    graph.append(_)
+
 dx=[-1,1,0,0]
 dy=[0,0,1,-1]
-a=True
 result=0
-def bfs(v,i):
-    q=deque()
-    q.append((v,i))
+def bfs(L):
     global result
-    while q:
-        v, i = q.popleft()
+    while L:
+        v, i = L.popleft()
         for  a in range(4):
             if 0<= v+dx[a] <n and 0<=i+dy[a] < m:
                 if graph[v+dx[a]][i+dy[a]] == 0:
-                    graph[v+dx[a]][i+dy[a]] = 1
-                    bfs(v+dx[a], i+dy[a])
+                    maximum=graph[v][i]+1
+                    graph[v+dx[a]][i+dy[a]] = maximum
+                    if maximum > result:
+                        result=maximum
+                    L.append((v+dx[a],i+dy[a]))
                 else:
                     continue
 
-bfs(n-1,m-1)
-for i in range(n):
-    if 0 in graph[i]:
-        print(-1)
-        a=False
-        break
-if a == True:
-    print(result)
+S_long=len(S)
+if S_long ==m*n:
+    a=2
+else:
+    for i in range(S_long):
+        bfs(S)
+
+    for i in range(n):
+        if 0 in graph[i]:
+            print(-1)
+            a=1
+            break
+if a == 0:
+    print(result-1)
+elif a==2 or a==3:
+    print(0)
     
-"""
-1.일단, result 값을 어떻게 추가해야할 지 생각
-2.또한 bfs를 n-1,m-1 좌표에서만 시작했던 것을 수정
-3.이미 토마토가 모두 익어있을 때는 어떻게 해야할지 생각.
+#python, pypy 모두 통과함.
+#기존에 copy 모듈의 deepcopy를 이용했을 때는 시간초과.
