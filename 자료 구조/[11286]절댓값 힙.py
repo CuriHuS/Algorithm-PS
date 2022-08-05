8503aa402b1c284f6c337e033efaef124cc3e9e2
CuriHuS <64942546+CuriHuS@users.noreply.github.com>
2022-08-05 22:31:24 +0900

    Create [11286]절댓값 힙.py

---
+++ 자료 구조/[11286]절댓값 힙.py
@@ -0,0 +1,13 @@
+import heapq
+import sys
+N=int(input())
+Q=[]
+for i in range(N):
+    a=int(sys.stdin.readline())
+    if a==0:
+        if len(Q)==0:
+            print(0)
+        else:
+            print(heapq.heappop(Q)[1])
+    else:
+        heapq.heappush(Q, [abs(a),a])
