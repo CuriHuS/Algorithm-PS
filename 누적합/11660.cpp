5e198f14c57b81cd226be658406c3b5d5acfd15a
CuriHuS <64942546+CuriHuS@users.noreply.github.com>
2024-02-16 15:46:22 +0900

    11660번: 구간 합 구하기 5

---
+++ 누적합/11660.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(false);
+
+	int N, M;
+	cin >> N >> M;
+	int** graph = new int* [N+1];
+	for (int i = 0; i <= N; i++) graph[i] = new int[N+1];
+	for (int i = 0; i <= N; i++) {
+		for (int j = 0; j <= N; j++) {
+			graph[i][j] = 0;
+		}
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			cin >> graph[i][j];
+			graph[i][j] += graph[i][j - 1];
+			graph[i][j] += graph[i - 1][j];
+			graph[i][j] -= graph[i - 1][j - 1];
+		}
+	}
+
+	int x1, y1, x2, y2, sum = 0;
+
+	for (int t = 0; t < M; t++) {
+		cin >> x1 >> y1 >> x2 >> y2;
+		sum = graph[x2][y2] - graph[x2][y1 - 1] - graph[x1 - 1][y2] + graph[x1 - 1][y1 - 1];
+		cout << sum << "\n";
+	}
+}
