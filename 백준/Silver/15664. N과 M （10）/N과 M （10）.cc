0c0c06697365196955699e0eb26d2888b0b953d9
CuriHuS <64942546+CuriHuS@users.noreply.github.com>
2025-08-18 17:34:30 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void dfs(int n, int m, int visited[], int arr[], vector<int> v, int cnt)
+{
+	if (cnt == m)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	int dup = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (visited[i] == 0 && dup != v[i] && v[i] >= arr[cnt-1])
+		{
+			visited[i] = 1;
+			arr[cnt] = v[i];
+			dup = v[i];
+			dfs(n, m, visited, arr, v, cnt + 1);
+			visited[i] = 0;
+		}
+	}
+	return;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> pool;
+	for (int i = 0; i < N; i++)
+	{
+		int a;
+		cin >> a;
+		pool.push_back(a);
+	}
+	sort(pool.begin(), pool.end());
+
+	int visited[9] = { 0 };
+	int arr[9] = { 0 };
+	dfs(N, M, visited, arr, pool, 0);
+}
